e02b5add88dd2b953b08d69fa4b3f3335769bfe2
Ataqah Ramadhan Widiyana Putra <ataqah.ramadhan@gmail.com>
2026-06-19 19:24:45 +0700

    chore: add minimal stubs for dashboard compatibility

---
+++ app/src/main/java/com/rentalkendaraan/ui/pelanggan/CariPelangganView.java
@@ -0,0 +1,3 @@
+package com.rentalkendaraan.ui.pelanggan;
+import com.rentalkendaraan.service.PelangganService; import com.rentalkendaraan.util.ConsoleFormatter; import java.util.Scanner;
+public class CariPelangganView { public CariPelangganView(PelangganService s, Scanner sc) {} public void show() { ConsoleFormatter.printInfo("Fitur pelanggan belum tersedia."); } }
---
+++ app/src/main/java/com/rentalkendaraan/ui/pelanggan/DaftarPelangganView.java
@@ -0,0 +1,3 @@
+package com.rentalkendaraan.ui.pelanggan;
+import com.rentalkendaraan.service.PelangganService; import com.rentalkendaraan.util.ConsoleFormatter; import java.util.Scanner;
+public class DaftarPelangganView { public DaftarPelangganView(PelangganService s, Scanner sc) {} public void show() { ConsoleFormatter.printInfo("Fitur pelanggan belum tersedia."); } }
